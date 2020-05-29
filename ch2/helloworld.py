11399e8d01c5731fb5051dc95f301779240c7c0c
john8895 <paiching01@gmail.com>
2020-05-29 16:04:58 +0800

    update

--- ch2/helloworld.py
+++ ch2/helloworld.py
@@ -1,7 +1,17 @@
-print("hello world")
+# print("hello world")
 
 # 註解
 # 代碼風格 PEP8
 # 代碼註釋
 
+# a = ['aaa','bbb']
+#
+# a.append('ccc')
+# print(a)
 
+# for car in a:
+#     print(car)
+
+students = ['Allen', 'Tom', 'Mayday', 'JJ', 'Jolin', 'Jay', 'Jam']
+for student in students:
+    print('Hi', student)
