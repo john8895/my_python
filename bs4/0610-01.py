99a4b272e36d8ddc81884f7beafbd28d6b021235
john8895 <paiching01@gmail.com>
2020-06-10 17:26:47 +0800

    update

--- bs4/0610-01.py
+++ bs4/0610-01.py
@@ -5,3 +5,7 @@ import json
 response = urlopen("https://www.google.com/doodles/json/2020/6?hl=zh_TW")
 doodles = json.load(response)
 print(doodles)
+
+# https://edu.tcfst.org.tw/edm/08C009/images/Python%E7%B6%B2%E8%B7%AF%E7%88%AC%E8%9F%B2%20.pdf
+# page 38
+
