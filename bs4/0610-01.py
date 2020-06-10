bc193a5afc2d312fe3b8ca589e0a24dcbf337e99
john8895 <paiching01@gmail.com>
2020-06-10 17:29:21 +0800

    update

--- bs4/0610-01.py
+++ bs4/0610-01.py
@@ -9,3 +9,8 @@ print(doodles)
 # https://edu.tcfst.org.tw/edm/08C009/images/Python%E7%B6%B2%E8%B7%AF%E7%88%AC%E8%9F%B2%20.pdf
 # page 38
 
+for d in doodles:
+    url = "https:", d["url"]
+    title = d["title"]
+    print("圖片標題:", title)
+    print("圖片網址:", url)
